2835f2cfd320f0be74d41d489c4ef695f15f50b9
Kanad Gupta <kgupta@umn.edu>
2021-05-11 17:02:51 -0500

    feat: add a bunch of clips, store them in public directory

---
+++ src/clips.js
@@ -0,0 +1,32 @@
+const clips = [
+  {
+    "audio": "policy.mp3",
+    "text": "culture eats policy for breakfast"
+  },
+  // {
+  //   "audio": "",
+  //   "text": "politics isn't about being somebody, it's about doing something (no audio yet)"
+  // },
+  {
+    "audio": "afford.mp3",
+    "text": "minneapolis is a place where people can afford to live in a safe and thriving neighborhood"
+  },
+  {
+    "audio": "affordable-housing.mp3",
+    "text": "you gotta be willing to stand up and say 'yes we're gonna put affordable housing here'"
+  },
+  {
+    "audio": "homelessness.mp3",
+    "text": "we don't have homelessness anymore"
+  },
+  {
+    "audio": "police.mp3",
+    "text": "believe it or not we actually have police that engage with the community"
+  },
+  {
+    "audio": "safety.mp3",
+    "text": "in order to have safety we need people to trust the police"
+  }
+]
+
+export default clips;
