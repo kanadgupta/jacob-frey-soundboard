d8e9be33cadcaa6e7e8919e088edc32596e2306c
Kanad Gupta <kgupta@umn.edu>
2021-05-12 13:40:11 -0500

    chore: move SEO to native index.html
    this is a very dumb setup on glitch's part... but Facebook and Twitter don't pick up on any of this at all since it's rendered via client-side JS!

--- src/app.jsx
+++ src/app.jsx
@@ -7,14 +7,9 @@ import "./styles/styles.css";
 // Where all of our pages come from
 import PageRouter from "./components/router.jsx";
 
-// The component that adds our Meta tags to the page
-import Seo from './components/seo.jsx';
-
-
 export default function Home() {
   return (
     <Router>
-      <Seo />
       <main role="main" className="wrapper">
         <div className="content">
           <PageRouter />
