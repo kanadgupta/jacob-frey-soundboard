8f437d3ee9a628e83756a8c7b63a3fa8fe785c1d
Kanad Gupta <kgupta@umn.edu>
2021-05-12 10:40:04 -0500

    feat: add a scroll to content thing at the top, and a content warning

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,14 +1,32 @@
-import * as React from "react";
+import React, { useEffect } from "react";
 
 import clips from "../clips";
 
 export default function Home() {
+  useEffect(() => {
+    // Stolen from: https://stackoverflow.com/a/7717572
+    document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+      anchor.addEventListener("click", function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute("href")).scrollIntoView({
+          behavior: "smooth",
+        });
+      });
+    });
+  });
+
   return (
     <>
+      <a href="#content">skip to stupid content ⬇️</a>
       <h1 className="title">
         hello! here's some of the stupid shit that the mayor of minneapolis has
         actually said:
       </h1>
+      <div id="content" className="content-warning">
+        ⚠️ content warning: audio clips containing cop-loving mayor running his stupid mouth. proceed
+        with caution! ⚠️
+      </div>
       <div className="item-container">
         {clips.map((clip, i) => {
           const audioRef = React.useRef();
