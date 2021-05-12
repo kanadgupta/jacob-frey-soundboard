612a7382ec5befdb3c0c1d86e04d312f5f1d22ce
Kanad Gupta <kgupta@umn.edu>
2021-05-11 20:54:44 -0500

    feat: add a bunch of clips, code formatting in clips.js

--- src/clips.js
+++ src/clips.js
@@ -1,32 +1,64 @@
 const clips = [
   {
-    "audio": "policy.mp3",
-    "text": "culture eats policy for breakfast"
-  },
-  // {
-  //   "audio": "",
-  //   "text": "politics isn't about being somebody, it's about doing something (no audio yet)"
-  // },
-  {
-    "audio": "afford.mp3",
-    "text": "minneapolis is a place where people can afford to live in a safe and thriving neighborhood"
+    audio: "policy.mp3",
+    text: "culture eats policy for breakfast",
   },
   {
-    "audio": "affordable-housing.mp3",
-    "text": "you gotta be willing to stand up and say 'yes we're gonna put affordable housing here'"
+    audio: "afford.mp3",
+    text: "minneapolis is a place where people can afford to live in a safe and thriving neighborhood",
   },
   {
-    "audio": "homelessness.mp3",
-    "text": "we don't have homelessness anymore"
+    audio: "abolish-i-said-no.mp3",
+    text: "two thousand people came to my home demanding that i abolish the police... i said no",
   },
   {
-    "audio": "police.mp3",
-    "text": "believe it or not we actually have police that engage with the community"
+    audio: "being-somebody.mp3",
+    text: "public service is not about being somebody it's about doing something",
   },
   {
-    "audio": "safety.mp3",
-    "text": "in order to have safety we need people to trust the police"
-  }
-]
+    audio: "homelessness5yr.mp3",
+    text: "we can end homeless in 5 years and i commit to do so",
+  },
+  {
+    audio: "marathon.mp3",
+    text: "[some shit about wanting to move here after running a marathon?]",
+  },
+  {
+    audio: "news-wrong-reasons.mp3",
+    text: "our city has been in the news for the wrong reasons and we need a fresh start",
+  },
+  {
+    audio: "promise.mp3",
+    text: "when i make a promise that we're going to hear people out,,, i do",
+  },
+  {
+    audio: "results.mp3",
+    text: "not rhetoric not lip service but results",
+  },
+  {
+    audio: "transparent.mp3",
+    text: "you can either have a mayor that's clear and honest and transparent[...] or you have what has happened the last 4 years",
+  },
+  {
+    audio: "alaykum.mp3",
+    text: "ٱلسَّلَامُ عَلَيْكُمْ‎",
+  },
+  {
+    audio: "affordable-housing.mp3",
+    text: "you gotta be willing to stand up and say 'yes we're gonna put affordable housing here'",
+  },
+  {
+    audio: "homelessness.mp3",
+    text: "we don't have homelessness anymore",
+  },
+  {
+    audio: "police.mp3",
+    text: "believe it or not we actually have police that engage with the community",
+  },
+  {
+    audio: "safety.mp3",
+    text: "in order to have safety we need people to trust the police",
+  },
+];
 
 export default clips;
