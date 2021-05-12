835ef62aad5a42f0e52950e5dab064f348eaa298
Kanad Gupta <kgupta@umn.edu>
2021-05-11 21:11:26 -0500

    copy updates

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,12 +4,45 @@ export default function About() {
   return (
     <div className="page">
       <h1 className="title">about this site</h1>
-      <p>hi. this site catalogs some (but not all, there's too much to keep track of) of the stupid shit the mayor of minneapolis says.</p>
       <p>
-        <a href="https://twitter.com/kanadgupta">@kanadgupta</a> built this with{" "}
-        <a href="https://reactjs.org/">react</a> and{" "}
-        <a href="https://vitejs.dev/">vite</a> on{" "}
-        <a href="https://glitch.com/">glitch</a>.
+        hi. jacob frey (aka pee pee poo poo bitch boy) is the mayor of
+        minneapolis.
+      </p>
+      <p>
+        this site catalogs some (but not all, there's too much to keep track of)
+        of the stupid shit that he says.
+      </p>
+      <p>
+        <a
+          target="_blank"
+          noreferrer
+          nooopener
+          href="https://twitter.com/kanadgupta"
+        >
+          @kanadgupta
+        </a>{" "}
+        built this with{" "}
+        <a target="_blank" noreferrer nooopener href="https://reactjs.org/">
+          react
+        </a>{" "}
+        and{" "}
+        <a target="_blank" noreferrer nooopener href="https://vitejs.dev/">
+          vite
+        </a>
+        . it was prototyped on{" "}
+        <a target="_blank" noreferrer nooopener href="https://glitch.com/">
+          glitch
+        </a>
+        . you can find the source code{" "}
+        <a
+          target="_blank"
+          noreferrer
+          nooopener
+          href="https://github.com/kanadgupta/jacob-frey-soundboard"
+        >
+          here
+        </a>
+        .
       </p>
     </div>
   );
